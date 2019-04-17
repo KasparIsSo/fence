fae31bfd5ed7c4b6c2f514cfc1763267596d0c53
KasparIsSo <kasparso421@gmail.com>
2019-04-17 02:58:50 -0400

    origin: false cors

--- dev/backend/src/index.js
+++ dev/backend/src/index.js
@@ -22,7 +22,8 @@ server.express.use((req, res, next) => {
 server.start(
   {
     cors: {
-      credentials: false
+      origin: false
+      // credentials: false
       // origin: ["https://fence-react-prod.herokuapp.com/"]
       // origin: process.env.FRONTEND_URL
     }
